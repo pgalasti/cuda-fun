8aaff037945a49ab5efd8bed1a23607b452fd3cd
Paul Galasti <pgalasti@gmail.com>
2025-12-11 17:45:26 -0500

    Hello world

---
+++ hello-world/hello-world.cu
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+__global__ void sayHelloFromGPULand() {
+  printf("Printf'ing off of the GPU!\n");
+}
+
+int main(int argc, char** argv) {
+
+  printf("Printf'ing off the CPU!\n");
+  
+  sayHelloFromGPULand<<<1,1>>>();
+  cudaDeviceSynchronize();
+
+  return 0;
+}
